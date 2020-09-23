5451801a0e9756506054313fa3cadf849fd0e972
Devin.Zhu <chooin@126.com>
2020-09-23 20:12:22 +0800

    fix: useShow useHide 不应该监听 AppState

--- src/hooks/use-hide.ts
+++ src/hooks/use-hide.ts
@@ -1,30 +1,15 @@
 import { EffectCallback, useEffect } from 'react';
-import { AppState, AppStateStatus } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 
 /**
- * 页面或 App 从前台变为后台时执行
+ * 页面从前台变为后台时执行
  */
 export default (effect: EffectCallback): void => {
   const navigation = useNavigation();
 
-  // ? 页面从前台变为后台时执行
   useEffect(() => {
     const unsubscribe = navigation.addListener('blur', effect);
 
     return unsubscribe;
   }, [navigation]);
-
-  // ? App 从前台变为后台时执行
-  const onChange = (state: AppStateStatus) => {
-    if (state !== 'active') {
-      effect();
-    }
-  };
-
-  useEffect(() => {
-    AppState.addEventListener('change', onChange);
-
-    return () => AppState.removeEventListener('change', onChange);
-  }, []);
 };
--- src/hooks/use-show.ts
+++ src/hooks/use-show.ts
@@ -1,30 +1,15 @@
 import { EffectCallback, useEffect } from 'react';
-import { AppState, AppStateStatus } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 
 /**
- * 页面或 APP 出现在前台时执行
+ * 页面出现在前台时执行
  */
 export default (effect: EffectCallback): void => {
   const navigation = useNavigation();
 
-  // ? 页面出现在前台时执行
   useEffect(() => {
     const unsubscribe = navigation.addListener('focus', effect);
 
     return unsubscribe;
   }, [navigation]);
-
-  // ? APP 出现在前台时执行
-  const onChange = (state: AppStateStatus) => {
-    if (state === 'active') {
-      effect();
-    }
-  };
-
-  useEffect(() => {
-    AppState.addEventListener('change', onChange);
-
-    return () => AppState.removeEventListener('change', onChange);
-  }, []);
 };
