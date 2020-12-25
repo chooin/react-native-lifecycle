4439d76b192310132e0cd6b4663e4a50b48fa638
Devin.Zhu <chooin@126.com>
2020-12-25 11:58:57 +0800

    fix: 时序问题

--- src/hooks/use-before-unload.ts
+++ src/hooks/use-before-unload.ts
@@ -1,14 +1,27 @@
-import { EffectCallback, useEffect } from 'react';
+import { EffectCallback, useEffect, useRef } from 'react';
 import { useNavigation } from '@react-navigation/native';
 
 /**
- * 页面销毁时执行
+ * 页面销毁前执行
  */
 export default (effect: EffectCallback): void => {
   const navigation = useNavigation();
+  const isBeforeRemove = useRef(false);
 
   useEffect(() => {
-    const unsubscribe = navigation.addListener('beforeRemove', effect);
+    const unsubscribe = navigation.addListener('beforeRemove', () => {
+      isBeforeRemove.current = true;
+    });
+
+    return unsubscribe;
+  }, [navigation]);
+
+  useEffect(() => {
+    const unsubscribe = navigation.addListener('blur', () => {
+      if (isBeforeRemove.current) {
+        effect();
+      }
+    });
 
     return unsubscribe;
   }, [navigation]);
