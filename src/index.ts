f09e82fe2cd62a0a174d4543796c5d5fc3af7628
Devin <chooin@126.com>
2022-02-20 23:22:42 +0800

    chore: rename file names

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
-import { useLoad } from './hooks/use-load';
-import { useShow } from './hooks/use-show';
-import { useHide } from './hooks/use-hide';
-import { useUnload } from './hooks/use-unload';
-import { useResize } from './hooks/use-resize';
-import { useAppActive } from './hooks/use-app-active';
-import { useAppInactive } from './hooks/use-app-inactive';
+import { useLoad } from './useLoad';
+import { useShow } from './useShow';
+import { useHide } from './useHide';
+import { useUnload } from './useUnload';
+import { useResize } from './useResize';
+import { useAppActive } from './useAppActive';
+import { useAppInactive } from './useAppInactive';
 
 export {
   useAppActive,
